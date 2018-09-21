7ebe9e0fb82a7d610504302c01200659dbb31bd2
Tommy Van Der Vaart <tommyvdv@hotmail.nl>
2018-09-21 20:53:43 +0200

    add changes

--- src/Contactformulier/send.php
+++ src/Contactformulier/send.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
-    $email_to = "bertjalon@hotmail.com";
+    $email_to = "bertjalon@gmail.com";
      
     $email_subject = "website html form submissions";
      
